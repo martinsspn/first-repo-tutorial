d1374e21b322bd6bff5373d2732dc0352e471b9e
martinsspn <martinsspn@gmail.com>
2019-02-21 10:03:19 -0300

    incrementando os arquivos hello world.py e readme.md

---
+++ src/hello_world.py
@@ -0,0 +1,6 @@
+# Arquivo de hello world para o tutorial de GIT
+
+print("Olá Mundo")
+print("Esté código está no meu primeiro repositório GIT")
+print("se quiser conferir é só acessar:")
+print("\nhttps://github.com/martinsspn/first-repo-tutorial\n")
